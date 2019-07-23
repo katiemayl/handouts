66fa125824d14a13a15d7c3d76501e6ef467a230
Katie May Laumann <klaumann@umces.edu>
2019-07-23 19:07:17 +0000

    Tuesday Assignment

--- worksheet-3.sh
+++ worksheet-3.sh
@@ -1,7 +1,7 @@
 # configure git
 
-git config --global user.name ...
-git config --global user.email ...
+git config --global user.name "Katie May Laumann"
+git config --global user.email "klaumann@umces.edu"
 git commit --no-edit --amend --reset-author
 
 # Link your local repository to the origin repository on GitHub, by
