b7b38395ce46345ec46be80d9350a61ce5b94bd4
Katie May Laumann <klaumann@umces.edu>
2019-07-24 15:05:56 +0000

    Regression Lesson Notes added

--- worksheet-6.R
+++ worksheet-6.R
@@ -3,16 +3,21 @@
 library(readr)
 library(dplyr)
 
-person <- read_csv(
-  file = ...,
-  col_types = cols_only(
-    ... = 'i',  # Age
-    ... = 'd',  # Wages or salary income past 12 months
-    ... = 'i',  # Educational attainment
-    ... = 'f',   # Sex
-    ... = 'f',  # Occupation recode based on 2010 OCC codes
-    ... = 'i')) # Usual hours worked per week past 12 months
+person1 <- read_csv(
+  file = 'data/census_pums/sample.csv')
+head(person1)
 
+person <- read_csv(
+  file = 'data/census_pums/sample.csv',
+  col_types = cols_only(
+    AGEP = 'i',  # Age
+    WAGP = 'd',  # Wages or salary income past 12 months
+    SCHL = 'i',  # Educational attainment
+    SEX = 'f',   # Sex
+    OCCP = 'f',  # Occupation recode based on 2010 OCC codes
+    WKHP = 'i')) # Usual hours worked per week past 12 months
+str(person)
+#what's the "i", "f", etc? type of factor
 person <- within(person, {
   SCHL <- factor(SCHL)
   levels(SCHL) <- list(
@@ -27,55 +32,77 @@ person <- within(person, {
     WAGP < max(WAGP, na.rm = TRUE))
 
 # Formula Notation
+# ~ means left is dependent var and indep var(s) are on right; data= nameof dataframe
+fit <- lm(
+  formula = WAGP ~ SCHL,
+  data = person)
+
+library(ggplot2)
+ggplot(person, aes(x=SCHL, y=WAGP))+ geom_boxplot()
+fit
+#need to fit model (above)
+# ??? look up more
+#lm function just does least squares
+
+
+#to not include a constant and force a 0 intercept, use "-1"
 
 fit <- lm(
-  formula = ...,
-  data = ...)
-
-fit <- lm(
-  ...,
+  log(WAGP) ~ SCHL,
   person)
+summary(fit)
 
 # Metadata matters
 
 fit <- lm(
-  ...,
+  log(WAGP) ~ AGEP,
   person)
+summary(fit)
+
+
+#agep is continuous data, not factor data
 
 # GLM families
 
-fit <- ...(...,
-  ...,
-  person)
+fit <- glm(log(WAGP) ~ SCHL,
+           family = gaussian,
+           data = person)
+summary(fit)
 
 # Logistic Regression
+fit <- glm(SEX ~ WAGP,
+           family = binomial,
+           data = person)
+#are wages a predictor of sex- is there a gender wage gap?
+summary(fit)
+levels(person$SEX)
 
-fit <- glm(...,
-  ...,
-  person)
+#have a pos wage coeff, and larger wages are associated w being a man
 
-...(fit, update(fit, ...), test = 'Chisq')
+anova(fit, update(fit, SEX ~ 1), test = 'Chisq')
+#lm and glm allow weighting of observations
 
 # Random Intercept
 
-library(...)
-fit <- ...(
-  ...,
+library(lme4)
+fit <- lmer(
+  log(WAGP) ~ (1|OCCP) + SCHL,
   data = person)
 
 # Random Slope
 
 fit <- lmer(
-  ...
+  log(WAGP) ~ (WKHP | SCHL),
   data = person)
+summary(fit)
 
 fit <- lmer(
   log(WAGP) ~ (WKHP | SCHL),
-  data = person,
-  control = ...)
+  data = person, 
+  control = lmerControl(optimizer = "bobyqa"))
 
 ggplot(person,
-  aes(x = WKHP, y = log(WAGP), color = SCHL)) +
+       aes(x = WKHP, y = log(WAGP), color = SCHL)) +
   geom_point() +
-  geom_line(...) +
+  geom_line(aes(y = predict(fit))) +
   labs(title = 'Random intercept and slope with lmer')
